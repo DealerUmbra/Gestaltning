777ab6f7607120d36e99f9f75eb52f0402167119
DealerUmbra <gaharion@gmail.com>
2017-04-04 11:08:10 +0200

    Uppdaterad 2017-04-04

---
+++ Map/Assets/HexCell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexCell : MonoBehaviour {
+
+    public HexCoordinates coordinates;
+    public Color color;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
