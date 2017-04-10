325948c41628a3e303136917758ec28521601787
DealerUmbra <gaharion@gmail.com>
2017-04-10 20:24:47 +0200

    Tutorial 5 klar

--- Map/Assets/Scripts/HexCell.cs
+++ Map/Assets/Scripts/HexCell.cs
@@ -4,15 +4,19 @@ public class HexCell : MonoBehaviour {
 
 	public HexCoordinates coordinates;
 
-	public Color color;
+	Color color;
 
 	public RectTransform uiRect;
+
+    public HexGridChunk chunk;
 
 	public int Elevation {
 		get {
 			return elevation;
 		}
 		set {
+            if (elevation == value)
+                return;
 			elevation = value;
 			Vector3 position = transform.localPosition;
 			position.y = value * HexMetrics.elevationStep;
@@ -24,8 +28,19 @@ public class HexCell : MonoBehaviour {
 			Vector3 uiPosition = uiRect.localPosition;
 			uiPosition.z = -position.y;
 			uiRect.localPosition = uiPosition;
+            Refresh();
 		}
 	}
+
+    public Color Color
+    {
+        get { return color; }
+        set {
+            if (color == value) return;
+            color = value;
+            Refresh();
+        }
+    }
 
 	public Vector3 Position {
 		get {
@@ -33,7 +48,7 @@ public class HexCell : MonoBehaviour {
 		}
 	}
 
-	int elevation;
+    int elevation = int.MinValue;
 
 	[SerializeField]
 	HexCell[] neighbors;
@@ -58,4 +73,18 @@ public class HexCell : MonoBehaviour {
 			elevation, otherCell.elevation
 		);
 	}
+
+    void Refresh()
+    {
+        if(chunk)
+        chunk.Refresh();
+        for(int i = 0; i<neighbors.Length; i++)
+        {
+            HexCell neighbor = neighbors[i];
+            if(neighbor != null && neighbor.chunk != chunk)
+            {
+                neighbor.chunk.Refresh();
+            }
+        }
+    }
 }
--- Map/Assets/Scripts/HexMapEditor.cs
+++ Map/Assets/Scripts/HexMapEditor.cs
@@ -11,7 +11,13 @@ public class HexMapEditor : MonoBehaviour {
 
 	Color activeColor;
 
+    bool applyColor;
+    bool applyElevation = true;
+    int brushSize;
+
 	public void SelectColor (int index) {
+        applyColor = index >= 0;
+        if(applyColor)
 		activeColor = colors[index];
 	}
 
@@ -36,13 +42,51 @@ public class HexMapEditor : MonoBehaviour {
 		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 		RaycastHit hit;
 		if (Physics.Raycast(inputRay, out hit)) {
-			EditCell(hexGrid.GetCell(hit.point));
+			EditCells(hexGrid.GetCell(hit.point));
 		}
 	}
 
+    public void ShowUI(bool visible)
+    {
+        hexGrid.ShowUI(visible);
+    }
+
+    public void SetApplyElevation (bool toggle)
+    {
+        applyElevation = toggle;
+    }
+
+    public void SetBrushSize(float size)
+    {
+        brushSize = (int)size;
+    }
+
 	void EditCell (HexCell cell) {
-		cell.color = activeColor;
-		cell.Elevation = activeElevation;
-		hexGrid.Refresh();
+        if (cell)
+        {
+            if (applyColor) { cell.Color = activeColor; }
+
+            if (applyElevation) { cell.Elevation = activeElevation; }
+        }
+		
 	}
+
+    void EditCells(HexCell center)
+    {
+        int centerX = center.coordinates.X;
+        int centerZ = center.coordinates.Z;
+        for (int r = 0, z = centerZ - brushSize; z <= centerZ; z++, r++)
+        {
+            for(int x = centerX - r; x <= centerX + brushSize; x++) {
+                EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+            }
+        }
+        for (int r = 0, z = centerZ + brushSize; z > centerZ; z--, r++)
+        {
+            for (int x = centerX - brushSize; x <= centerX + r; x++)
+            {
+                EditCell(hexGrid.GetCell(new HexCoordinates(x, z)));
+            }
+        }
+    }
 }
